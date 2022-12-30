2c24b52f79fe6ba28dd00da424c40e54a1b832df
Karl Marx Manzano <karlmarxmanzano@gmail.com>
2022-12-30 15:17:32 +0800

    Add error dialog and login view error handling

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:notes/constants/routes.dart';
+import 'package:notes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -69,13 +68,26 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found.');
+                  await showErrorDialog(
+                    context,
+                    'User not found.',
+                  );
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('Wrong password.');
+                  await showErrorDialog(
+                    context,
+                    'Wrong credentials.',
+                  );
                 } else {
-                  devtools.log('Something else happened.');
-                  devtools.log(e.code);
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text('Login'),
