49010499e9ea8ec666c7647923e19340ee8a863f
Karl Marx Manzano <karlmarxmanzano@gmail.com>
2022-12-30 14:32:12 +0800

    Add notes route and redirect user to notes when successful login

--- lib/view/register_view.dart
+++ lib/view/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -61,17 +62,17 @@ class _RegisterViewState extends State<RegisterView> {
                   email: email,
                   password: password,
                 );
-                print(userCredentials);
+                devtools.log(userCredentials.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
-                  print('Weak password.');
+                  devtools.log('Weak password.');
                 } else if (e.code == 'email-already-in-use') {
-                  print('Email is already in use.');
+                  devtools.log('Email is already in use.');
                 } else if (e.code == 'invalid-email') {
-                  print('Invalid email.');
+                  devtools.log('Invalid email.');
                 } else {
-                  print('Something else happened.');
-                  print(e.code);
+                  devtools.log('Something else happened.');
+                  devtools.log(e.code);
                 }
               }
             },
